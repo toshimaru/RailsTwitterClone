f749f894b761ae569415b16756eac1e57d24bfae
toshi <me@toshimaru.net>
2014-03-17 08:51:02 -0700

    Add root_path capybara test

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "UserPages" do
-
   subject { page }
 
   describe "GET /" do
     before { visit root_path }
-    it { have_content('Welcome to Twitter Clone') }
+    it { should have_content('Welcome to Twitter Clone') }
+    it { should have_link('Sign In') }
+    it { should have_link('Sign Up') }
   end
 end
