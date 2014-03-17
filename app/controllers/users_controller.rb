fd8d6d783232d8ed951fe7fe00a5207e80feb5cb
toshi <me@toshimaru.net>
2014-03-17 10:05:57 -0700

    Add signing method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def signin
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
