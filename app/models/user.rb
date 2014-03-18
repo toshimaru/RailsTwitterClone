dc38fdc0362524b722892409d591739b105d0fe4
toshi <me@toshimaru.net>
2014-03-17 19:17:15 -0700

    Add login token logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,4 +10,18 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_confirmation_of :password, if: lambda { |m| m.password.present? }
   validates :password, length: { minimum: 6 }
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
