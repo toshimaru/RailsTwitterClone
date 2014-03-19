e0052efc11089bfd76f3eeef7d4af1d18617cf29
toshi <me@toshimaru.net>
2014-03-18 20:32:52 -0700

    Add spec utility

---
+++ spec/support/utilities.rb
@@ -0,0 +1,13 @@
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
