df85b1c7b50e6556ee976061707e8d294c6da295
toshi <me@toshimaru.net>
2014-03-19 17:36:58 -0700

    Rspec only accepts new syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,4 +44,10 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Rspec only accepts new syntax
+  # ref. http://betterspecs.org/#expect
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
