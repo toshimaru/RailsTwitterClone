c586b0733bb84bcfaae57f884c8dfe8e90aa49d6
toshi <me@toshimaru.net>
2014-03-19 20:20:24 -0700

    Add signed_in_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -67,4 +68,9 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    def signed_in_user
+      redirect_to signin_url, notice: 'Please singin' unless signed_in?
+    end
+
 end
