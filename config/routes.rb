adef0a875c189f9523595d127a2fd4fecc26cce0
toshi <me@toshimaru.net>
2014-03-21 15:58:58 -0700

    Add micropost route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails4TwitterClone::Application.routes.draw do
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
