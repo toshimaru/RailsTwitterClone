6462aaee27de0448a66201407893fb1b9bdd3a90
toshi <me@toshimaru.net>
2014-03-23 04:56:29 -0700

    Add common method to show title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "Twitter Clone"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
