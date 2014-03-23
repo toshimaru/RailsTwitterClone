3ac1d41414408279c0389fca13f45a0cdcde0d7a
toshi <me@toshimaru.net>
2014-03-23 04:57:30 -0700

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails4TwitterClone::Application.routes.draw do
-  resources :users
-  resources :sessions,   only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root   'static_pages#home'
+  root 'static_pages#home'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
   get    'signup'   => 'users#new'
   get    'signin'   => 'sessions#new'
   delete 'signout'  => 'sessions#destroy'
-
   get    'about'    => 'static_pages#about'
 
   # Example of regular route:
