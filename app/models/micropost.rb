34962029bc68aa50f0b736836be13e25b983143d
toshi <me@toshimaru.net>
2014-03-24 08:22:32 -0700

    Use subquery

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,7 +7,8 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
 
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user)
   end
 end
