3fbb2a622531ebc1db73322e2159ca60fb685f09
toshi <me@toshimaru.net>
2014-03-23 19:39:51 -0700

    Add `screen_name`

--- db/migrate/20140307124026_create_users.rb
+++ db/migrate/20140307124026_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
+      t.string :screen_name
       t.string :email
       t.string :password
 
