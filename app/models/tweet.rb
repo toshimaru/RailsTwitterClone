eec55ac8e6ae30d7b399bc6749827d4bed6c6d7b
toshi <me@toshimaru.net>
2014-11-28 02:54:53 +0900

    Arel query

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,14 +1,19 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
 
-  default_scope -> { order('created_at DESC') }
+  default_scope -> { order(created_at: :desc) }
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
 
   def self.from_users_followed_by(user)
-    followed_user_ids = "SELECT followed_id FROM relationships
-                         WHERE follower_id = :user_id"
-    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user)
+    r = Relationship.arel_table
+    t = Tweet.arel_table
+    sub_query = t[:user_id].in(r.where(r[:follower_id].eq(user.id)).project(r[:followed_id]))
+    where(sub_query.or(t[:user_id].eq(user.id)))
+    # - No Arel -
+    # followed_user_ids = "SELECT followed_id FROM relationships
+    # WHERE follower_id = :user_id"
+    # where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user)
   end
 end
