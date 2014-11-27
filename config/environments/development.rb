6ba8c7e0478b4fa4d2693bd2f070bde96bd6fe00
toshi <me@toshimaru.net>
2014-11-28 02:54:16 +0900

    No bullet alert

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,6 @@ Rails4TwitterClone::Application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
   end
