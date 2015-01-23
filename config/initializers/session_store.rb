54312bc78a98533a1fc77b3272205e6911828635
toshi <me@toshimaru.net>
2015-01-24 00:23:01 +0900

    Bump configurations for Rails 4.2 :star:

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails4TwitterClone::Application.config.session_store :cookie_store, key: '_rails-4-twitter-clone_session'
+Rails.application.config.session_store :cookie_store, key: '_rails-4-twitter-clone_session'
