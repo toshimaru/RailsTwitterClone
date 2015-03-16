b673af55518464928269aa565dd60c9f286273a7
toshi <me@toshimaru.net>
2015-03-16 13:27:00 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20140725142845) do
 
-  create_table "relationships", force: true do |t|
+  create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
     t.datetime "created_at"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20140725142845) do
   add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
-  create_table "tweets", force: true do |t|
-    t.string   "content"
+  create_table "tweets", force: :cascade do |t|
+    t.string   "content",    limit: 255
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -33,13 +33,13 @@ ActiveRecord::Schema.define(version: 20140725142845) do
 
   add_index "tweets", ["user_id", "created_at"], name: "index_tweets_on_user_id_and_created_at"
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_token"
+    t.string   "remember_token",  limit: 255
     t.string   "slug"
   end
 
