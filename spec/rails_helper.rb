db2e1f6b099173da9368056f7a3e7e9015d3526a
toshi <me@toshimaru.net>
2016-02-06 00:43:54 +0900

    More screenshots

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,10 @@ require 'rspec/rails'
 
 require "capybara/poltergeist"
 Capybara.javascript_driver = :poltergeist
+Capybara::ScreenshotConfig.configure do |config|
+  # config.save_dir = "screenshot"
+  config.full = true
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
