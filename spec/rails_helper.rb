b7130a34a17d8c95cfac66297febc60d5a6e731d
Toshimaru <me@toshimaru.net>
2017-08-18 00:01:38 +0900

    Fix Coverage (#83)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,14 +4,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
 # coverage with simplecov and codeclimate
+# NOTE: place `SimpleCov.start` before `require 'rspec/rails'`
 require 'simplecov'
-require 'capybara/poltergeist'
-
 SimpleCov.start 'rails'
+require 'rspec/rails'
+
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
