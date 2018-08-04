fead6c2b689ab22aa7f59e948b87a2d8b0b68537
toshimaru <me@toshimaru.net>
2018-08-04 17:30:06 +0900

    Capybara.javascript_driver = :selenium

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,8 +14,8 @@ SimpleCov.start "rails"
 require "rspec/rails"
 
 # Add additional requires below this line. Rails is not loaded until this point!
-require "capybara/poltergeist"
-Capybara.javascript_driver = :poltergeist
+require "selenium-webdriver"
+Capybara.javascript_driver = :selenium
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
