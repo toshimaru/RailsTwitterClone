36fbce43c0852db80512a8609d335ac520ba99c1
Toshimaru <me@toshimaru.net>
2019-08-23 11:45:22 +0900

    Update schema.rb (#491)

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725142845) do
+ActiveRecord::Schema.define(version: 2014_07_25_142845) do
 
   create_table "relationships", force: :cascade do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
-
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "tweets", force: :cascade do |t|
-    t.string   "content"
-    t.integer  "user_id"
+    t.string "content"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["user_id", "created_at"], name: "index_tweets_on_user_id_and_created_at"
   end
-
-  add_index "tweets", ["user_id", "created_at"], name: "index_tweets_on_user_id_and_created_at"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_token"
-    t.string   "slug"
+    t.string "remember_token"
+    t.string "slug"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_token"], name: "index_users_on_remember_token"
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
-  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
 
 end
