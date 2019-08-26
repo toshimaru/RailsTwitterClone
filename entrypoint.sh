16c92e85b646cd4046a6d954a1d6e398432d9a69
toshimaru <me@toshimaru.net>
2019-08-27 06:19:08 +0900

    fix app path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
+
