ec164fa4d6637cfa547b6b5190a0bce663e9dd0a
toshimaru <me@toshimaru.net>
2019-09-04 01:11:56 +0900

    Specify driven_by for SystemSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,4 +75,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
 end
