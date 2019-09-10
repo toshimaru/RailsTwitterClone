e0850ed2fa02afdd3bae486285c10777d698625f
toshimaru <me@toshimaru.net>
2019-09-11 02:01:40 +0900

    Perfomance enhancement: Fix N+1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @tweet = current_user.tweets.new
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.includes(:user).paginate(page: params[:page])
     end
   end
 
