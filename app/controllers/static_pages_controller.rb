93075a344c5b149461b968c005188a1f3acab0e5
toshimaru <me@toshimaru.net>
2019-09-11 02:15:10 +0900

    form_for -> form_with on tweet

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@
 class StaticPagesController < ApplicationController
   def home
     if signed_in?
-      @tweet = current_user.tweets.new
       @feed_items = current_user.feed.includes(:user).paginate(page: params[:page])
     end
   end
