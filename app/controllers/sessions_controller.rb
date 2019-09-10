dde433364af021d3bc6f7fa4ee33798a11b97d12
toshimaru <me@toshimaru.net>
2019-09-10 23:25:44 +0900

    Use form_with on login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
       log_in user
       redirect_back_or root_url
     else
       flash.now[:danger] = "Invalid email/password combination"
-      render "new"
+      render :new
     end
   end
 
