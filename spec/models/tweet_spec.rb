d7b79bb22cef17c30dff664780c28456ec6183a6
toshimaru <me@toshimaru.net>
2019-09-24 23:18:22 +0900

    fixtures :users

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,7 +3,9 @@
 require "rails_helper"
 
 RSpec.describe Tweet, type: :model do
-  let(:user) { FactoryBot.create(:user) }
+  fixtures :users
+
+  let(:user) { users(:fixture_user_1) }
 
   subject(:tweet) { user.tweets.build(content: "Lorem ipsum") }
 
@@ -16,16 +18,16 @@ RSpec.describe Tweet, type: :model do
 
   describe "when user_id is not present" do
     before { tweet.user_id = nil }
-    it { should_not be_valid }
+    it { should be_invalid }
   end
 
   describe "with blank content" do
     before { tweet.content = " " }
-    it { should_not be_valid }
+    it { should be_invalid }
   end
 
   describe "with content that is too long" do
     before { tweet.content = "a" * 141 }
-    it { should_not be_valid }
+    it { should be_invalid }
   end
 end
