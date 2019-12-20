607a86c67934e5920c9d3e06aa0f3599c7e9901c
toshimaru <me@toshimaru.net>
2019-12-21 00:17:58 +0900

    Set inverse_of option

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
+                                  inverse_of: :user,
                                   dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship",
                                    foreign_key: "followed_id",
+                                   inverse_of: :user,
                                    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
