2d3db2b7dfd15d5fb9951a21513aded454733f67
Toshimaru <me@toshimaru.net>
2019-12-21 01:16:45 +0900

    Fix some of rubocop-rails rubocop warnings (#626)
    * gem "rubocop-rails"
    
    * require rubocop-rails
    
    * Update .rubocop.yml
    
    * Revert "gem "rubocop-rails""
    
    This reverts commit fe3f529e7831a3ed951277a9c3dda5dddcf81ed0.
    
    * Set inverse_of option
    
    * validates_confirmation_of -> validates & confirmation
    
    * Use http status symbol
    
    * Remove wrap method
    
    * Revert rubocop.yml change
    
    * Fix inverse_of option

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
+                                  inverse_of: :follower,
                                   dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship",
                                    foreign_key: "followed_id",
+                                   inverse_of: :followed,
                                    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
@@ -22,8 +24,8 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   # See implementation: https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rb
   has_secure_password
-  validates_confirmation_of :password, if: ->(m) { m.password.present? }
   validates :password, length: { minimum: 6 }
+  validates :password, confirmation: true, if: ->(u) { u.password.present? }
   validates :slug, uniqueness: true
 
   def to_param
