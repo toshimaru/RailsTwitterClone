acc0e0d8b6d7c61783e5560351cc27583bfc7055
toshimaru <me@toshimaru.net>
2021-02-08 09:26:17 +0900

    require rails-ujs
    > jQuery was required by default in earlier versions of Rails to provide features like data-remote, data-confirm and other parts of Rails' Unobtrusive JavaScript offerings. It is no longer required, as the UJS has been rewritten to use plain, vanilla JavaScript. This code now ships inside of Action View as rails-ujs.
    
    ref. https://guides.rubyonrails.org/5_1_release_notes.html#jquery-no-longer-a-default-dependency

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
