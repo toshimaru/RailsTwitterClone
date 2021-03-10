0b5dd0b927e5e93ac78eb6e2cb7492b6505af4b7
Toshimaru <me@toshimaru.net>
2021-03-11 00:24:56 +0900

    Use `@rails/ujs` via webpacker (#1045)
    * Remove `require rails-ujs` from Sprocket
    
    * yarn add `@rails/ujs`
    
    ref. https://github.com/rails/rails/blob/4607f13c88fdc773a3ee69101bcb6ebe41ed20ce/actionview/package.json
    
    * Remove vendor/assets directory
    
    * javascript_pack_tag -> javascript_include_tag
    
    * Enable `@rails/ujs`
    
    * Remove `require_tree .` since there's no JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,4 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require turbolinks
-//= require_tree .
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,6 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import Rails from '@rails/ujs';
+Rails.start();
