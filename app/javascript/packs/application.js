708adcb601675abd2679fd07a4854d034440a24f
toshimaru <me@toshimaru.net>
2021-03-13 18:12:27 +0900

    Move some styles from sprockets to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,5 +15,7 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import Rails from '@rails/ujs';
-Rails.start();
+import 'stylesheets/application.scss'
+
+import Rails from '@rails/ujs'
+Rails.start()
