4af169d7d6c6f1e375662ba354fa58c2572525a5
Toshimaru <me@toshimaru.net>
2021-03-20 11:00:23 +0900

    Re-implement user login (#1055)
    * Add reset_session after authenticate
    
    ref. https://guides.rubyonrails.org/security.html#session-fixation-countermeasures
    
    * Rename:  signed_in?→logged_in?
    
    * Add reset_session on log_out
    
    * Simplify link_to Profile
    
    * Use plain session instead of remember token in Cookie
    
    * Remove store_location
    
    since it doesn't work as long as app calls reset_session
    after login
    
    * Remove redirect_back_or helper method
    
    * Use session mock instead of using cookie in RSpec
    
    * Remove `return_to` related spec
    
    * signed_in_user→logged_in_user
    
    * log_in→log_in_as

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,43 +2,30 @@
 
 module SessionsHelper
   def log_in(user)
-    remember_token = User.new_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.hexdigest(remember_token))
-    @current_user = user
+    session[:user_id] = user.id
   end
 
-  def signed_in?
+  def logged_in?
     !current_user.nil?
   end
 
   def log_out
-    current_user.update_attribute(:remember_token, User.hexdigest(User.new_token))
-    cookies.delete(:remember_token)
+    reset_session
     @current_user = nil
   end
 
   def current_user
-    remember_token = User.hexdigest(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
   def current_user?(user)
     user == current_user
   end
 
-  def store_location
-    session[:return_to] = request.url if request.get?
-  end
-
-  def redirect_back_or(default)
-    redirect_to(session[:return_to] || default)
-    session.delete(:return_to)
-  end
-
-  def signed_in_user
-    unless signed_in?
-      store_location
+  def logged_in_user
+    unless logged_in?
       redirect_to login_path, notice: "Please sign in."
     end
   end
