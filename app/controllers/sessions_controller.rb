81dd52edda4034a3b8965d5c0f061f485b745017
toshimaru <me@toshimaru.net>
2021-03-21 01:20:27 +0900

    Call User#remember and set cookies after autheinticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session
+      remember(user)
       log_in(user)
       redirect_to root_url
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,4 +29,10 @@ module SessionsHelper
       redirect_to login_path, notice: "Please sign in."
     end
   end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 end
