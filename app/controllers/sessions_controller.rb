6dc543b8511714df67fb7f6d1f7283cc2210e0c0
toshimaru <me@toshimaru.net>
2021-03-20 18:35:28 +0900

    Add `scope: :session`

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       reset_session
       log_in(user)
       redirect_to root_url
