e3bf3a25c1d264b2fa39ce55c3a8358475a8a43e
toshimaru <me@toshimaru.net>
2021-03-20 11:51:39 +0900

    Remove comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,12 +60,10 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find_by!(slug: params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :slug)
     end
