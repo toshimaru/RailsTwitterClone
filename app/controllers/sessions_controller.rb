cd7a580fa94beb9819b08e23c80e7f827bd12564
toshimaru <me@toshimaru.net>
2021-03-23 09:25:13 +0900

    Handle remember_me param

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session
-      remember(user)
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       log_in(user)
       redirect_to root_url
     else
