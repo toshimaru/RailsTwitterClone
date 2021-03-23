5982e1284abeb1c3b573246dc22514354f389a20
toshimaru <me@toshimaru.net>
2021-03-24 02:41:41 +0900

    Remove `resources :sessions` routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  resources :sessions,      only: [:new, :create, :destroy]
   resources :tweets,        only: [:index, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
 
