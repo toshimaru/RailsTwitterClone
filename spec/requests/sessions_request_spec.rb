968bfb26cf65807d58ef1c8054dcc3c709370047
toshimaru <me@toshimaru.net>
2021-03-23 09:41:36 +0900

    Test: POST /create

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -13,6 +13,27 @@ RSpec.describe "/sessions", type: :request do
     end
   end
 
+  describe "POST /create" do
+    it "redirects to root" do
+      post login_path, params: { session: { email: user.email, password: "password" } }
+      expect(response).to redirect_to(root_url)
+    end
+
+    it "creates a new session with remember_token cookie" do
+      post login_path, params: {
+        session: { email: user.email, password: "password", remember_me: "1" }
+      }
+      expect(cookies[:remember_token]).to be_present
+    end
+
+    it "creates a new session without remember_token cookie" do
+      post login_path, params: {
+        session: { email: user.email, password: "password", remember_me: "0" }
+      }
+      expect(cookies[:remember_token]).to be_nil
+    end
+  end
+
   describe "DELETE /destroy" do
     context "after logged in" do
       before { log_in_as(user) }
