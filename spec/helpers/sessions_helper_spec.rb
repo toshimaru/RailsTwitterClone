3d3bd70bdd354d597338cbc5c9e409daaf20ea17
toshimaru <me@toshimaru.net>
2021-03-23 11:52:32 +0900

    Test: SessionsHelper#current_user

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -3,4 +3,15 @@
 require "rails_helper"
 
 RSpec.describe SessionsHelper, type: :helper do
+  fixtures :users
+
+  let(:user) { users(:fixture_user_1) }
+
+  describe "#current_user" do
+    before { remember(user) }
+
+    it "returns remembered user" do
+      expect(current_user).to eq user
+    end
+  end
 end
