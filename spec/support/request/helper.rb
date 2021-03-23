ee9e2955f07d8e6fb7c303a48a96f7e9289dccfd
toshimaru <me@toshimaru.net>
2021-03-23 09:27:52 +0900

    Add password, remember_me key args

--- spec/support/request/helper.rb
+++ spec/support/request/helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module RequestSpecHelper
-  def log_in_as(user)
-    post login_path, params: { session: { email: user.email, password: "password" } }
+  def log_in_as(user, password: "password", remember_me: "1")
+    post login_path, params: {
+      session: { email: user.email, password: password, remember_me: remember_me }
+    }
   end
 end
