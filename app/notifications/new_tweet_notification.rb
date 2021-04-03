9604551c8e30288bf04effb5ad539dbba3d289dd
toshimaru <me@toshimaru.net>
2021-04-03 18:07:56 +0900

    rails g noticed:notification NewTweetNotification

---
+++ app/notifications/new_tweet_notification.rb
@@ -0,0 +1,27 @@
+# To deliver this notification:
+#
+# NewTweetNotification.with(post: @post).deliver_later(current_user)
+# NewTweetNotification.with(post: @post).deliver(current_user)
+
+class NewTweetNotification < Noticed::Base
+  # Add your delivery methods
+  #
+  deliver_by :database
+  # deliver_by :email, mailer: "UserMailer"
+  # deliver_by :slack
+  # deliver_by :custom, class: "MyDeliveryMethod"
+
+  # Add required params
+  #
+  # param :post
+
+  # Define helper methods to make rendering easier.
+  #
+  # def message
+  #   t(".message")
+  # end
+  #
+  # def url
+  #   post_path(params[:post])
+  # end
+end
