d6f5fd52e3a40673fc51d286d2f74abfc9db5c1b
toshimaru <me@toshimaru.net>
2021-04-04 14:47:27 +0900

    create_join_table relationships

---
+++ db/migrate/20210404000002_create_join_table_relationships.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableRelationships < ActiveRecord::Migration[6.0]
+  def change
+    create_join_table :followers, :followeds, table_name: :relationships do |t|
+      t.index [:follower_id, :followed_id], unique: true
+      t.index :followed_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_000001) do
+ActiveRecord::Schema.define(version: 2021_04_04_000002) do
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "relationships", id: false, force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "tweets", force: :cascade do |t|
