04e3521e750e696041747c2b86250b7e8ff77e6d
toshimaru <me@toshimaru.net>
2021-04-04 19:13:25 +0900

    before_action :set_relationship_user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,20 @@
 
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
+  before_action :set_relationship_user
 
   def create
-    @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
     redirect_to @user
   end
 
   def destroy
-    @user = User.find(params[:relationship][:followed_id])
     current_user.unfollow(@user)
     redirect_to @user
   end
+
+  private
+    def set_relationship_user
+      @user = User.find(params[:relationship][:followed_id])
+    end
 end
