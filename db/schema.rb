12ffa54ffd2ebe5dd51fd622cd3b1d653fb95f41
toshimaru <me@toshimaru.net>
2021-04-11 13:13:50 +0900

    rails generate noticed:model → db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_000002) do
+ActiveRecord::Schema.define(version: 2021_04_11_041257) do
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "recipient_type", null: false
+    t.integer "recipient_id", null: false
+    t.string "type", null: false
+    t.json "params"
+    t.datetime "read_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["read_at"], name: "index_notifications_on_read_at"
+    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient_type_and_recipient_id"
+  end
 
   create_table "relationships", id: false, force: :cascade do |t|
     t.integer "follower_id", null: false
---
+++ spec/factories/notifications.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :notification do
+    recipient { nil }
+    type { "" }
+    params { "" }
+    read_at { "2021-04-11 13:12:57" }
+  end
+end
