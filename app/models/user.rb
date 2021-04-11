ad46a73332f719e2eb101e01836d4fa32dd0b2cd
toshimaru <me@toshimaru.net>
2021-04-11 17:38:19 +0900

    has_many :notifications, as: :recipient

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
                                    dependent: :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :notifications, as: :recipient
 
   before_save { self.email = email.downcase }
 
