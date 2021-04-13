a7bbce6d53df7c88e97b30c1ae0ed0d77f9ba5e3
toshimaru <me@toshimaru.net>
2021-04-14 08:44:16 +0900

    rubocop -A for db and config directory

--- db/migrate/20210101000000_check_existing_migrations_for_rails6.rb
+++ db/migrate/20210101000000_check_existing_migrations_for_rails6.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 # Database schema has been newly created in Rails 6.
 #
-# This migration file checks there's an old migration invoked in the past and 
+# This migration file checks there's an old migration invoked in the past and
 # if any, raise an error to enforce `db:reset`.
-# 
+#
 # CAUTION: ALL DATA WILL BE CLEARED IF YOU RESET DATABASE.
 class CheckExistingMigrationsForRails6 < ActiveRecord::Migration[6.0]
-  OLD_MIGRATION_VERSIONS = [20140307124026, 20140315090930, 20140315103002, 20140317214823, 20140319034048, 
+  OLD_MIGRATION_VERSIONS = [20140307124026, 20140315090930, 20140315103002, 20140317214823, 20140319034048,
                             20140323031217, 20140326232214, 20140725142845, 20201231000000]
 
   def change
