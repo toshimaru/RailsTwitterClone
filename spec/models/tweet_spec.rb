b9b33740a983ab76060ff5d87c4fb43f8daf6228
toshimaru <me@toshimaru.net>
2021-07-28 09:10:27 +0900

    Refactoring tweet_spec

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -9,25 +9,33 @@ RSpec.describe Tweet, type: :model do
 
   subject(:tweet) { user.tweets.build(content: "Lorem ipsum") }
 
-  it { should respond_to(:content) }
-  it { should respond_to(:user_id) }
-  it { should respond_to(:user) }
-  it { expect(tweet.user).to eq(user) }
-
-  it { should be_valid }
-
-  describe "when user_id is not present" do
-    before { tweet.user_id = nil }
-    it { should be_invalid }
+  describe "attributes" do
+    it { should respond_to(:content) }
+    it { should respond_to(:user_id) }
   end
 
-  describe "with blank content" do
-    before { tweet.content = " " }
-    it { should be_invalid }
+  describe "associations" do
+    it { should respond_to(:user) }
+    it { expect(tweet.user).to eq(user) }
+    it { should respond_to(:image) }
   end
 
-  describe "with content that is too long" do
-    before { tweet.content = "a" * 141 }
-    it { should be_invalid }
+  describe "validations" do
+    it { should be_valid }
+
+    describe "when user_id is not present" do
+      before { tweet.user_id = nil }
+      it { should be_invalid }
+    end
+
+    describe "with blank content" do
+      before { tweet.content = " " }
+      it { should be_invalid }
+    end
+
+    describe "with content that is too long" do
+      before { tweet.content = "a" * 141 }
+      it { should be_invalid }
+    end
   end
 end
