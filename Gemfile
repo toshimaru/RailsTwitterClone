c08e503aa232820b23c2b0576618033ccbc8a03a
Toshimaru <me@toshimaru.net>
2021-07-31 21:28:26 +0900

    feat: Image validation (#1198)
    * gem "active_storage_validations"
    
    * Add image validation
    
    * Accept only image
    
    * Specify image format
    
    * Refactoring tweet_spec
    
    * test: invalid image spec

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem "rails", "~> 6.0.4"
 
+gem "active_storage_validations"
 gem "bcrypt"
 gem "bootsnap", require: false
 gem "bootstrap-sass"
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -9,25 +9,39 @@ RSpec.describe Tweet, type: :model do
 
   subject(:tweet) { user.tweets.build(content: "Lorem ipsum") }
 
-  it { should respond_to(:content) }
-  it { should respond_to(:user_id) }
-  it { should respond_to(:user) }
-  it { expect(tweet.user).to eq(user) }
-
-  it { should be_valid }
-
-  describe "when user_id is not present" do
-    before { tweet.user_id = nil }
-    it { should be_invalid }
+  describe "attributes" do
+    it { is_expected.to respond_to(:content) }
+    it { is_expected.to respond_to(:user_id) }
   end
 
-  describe "with blank content" do
-    before { tweet.content = " " }
-    it { should be_invalid }
+  describe "associations" do
+    it { is_expected.to respond_to(:user) }
+    it { expect(tweet.user).to eq(user) }
+    it { is_expected.to respond_to(:image) }
   end
 
-  describe "with content that is too long" do
-    before { tweet.content = "a" * 141 }
-    it { should be_invalid }
+  describe "validations" do
+    it { is_expected.to be_valid }
+
+    describe "when user_id is not present" do
+      before { tweet.user_id = nil }
+      it { is_expected.to be_invalid }
+    end
+
+    describe "with blank content" do
+      before { tweet.content = " " }
+      it { is_expected.to be_invalid }
+    end
+
+    describe "with content that is too long" do
+      before { tweet.content = "a" * 141 }
+      it { is_expected.to be_invalid }
+    end
+
+    describe "with invalid image" do
+      let(:image) { fixture_file_upload(file_fixture("empty.txt")) }
+      before { tweet.image.attach(image) }
+      it { is_expected.to be_invalid }
+    end
   end
 end
