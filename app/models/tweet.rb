25bc98639ff0dbece33b2a3db091f9502f5e75a0
toshimaru <me@toshimaru.net>
2021-08-31 09:52:19 +0900

    List resized image

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,4 +10,8 @@ class Tweet < ApplicationRecord
   validates :user_id, presence: true
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
                     size: { less_than: 5.megabytes, message: "should be less than 5MB" }
+
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
