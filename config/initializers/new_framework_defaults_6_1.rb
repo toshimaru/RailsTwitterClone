3022f3078b419f3b2995983d6c4a1586cc0c4cdc
toshimaru <me@toshimaru.net>
2022-02-22 04:06:49 +0900

    Remove `local: true`
    since form_with generates non-remote forms

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -45,9 +45,6 @@
 # support for granular connection swapping.
 # Rails.application.config.active_record.legacy_connection_handling = false
 
-# Make `form_with` generate non-remote forms by default.
-# Rails.application.config.action_view.form_with_generates_remote_forms = false
-
 # Set the default queue name for the analysis job to the queue adapter default.
 # Rails.application.config.active_storage.queues.analysis = nil
 
