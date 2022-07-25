60a1c01c769859f3cb540378752d24c8fba4729a
Toshimaru <me@toshimaru.net>
2022-07-26 00:06:31 +0900

    Bump ActiveRecord::Migration from 6.0 to 6.1
    To fix the error:
    
    ```
    Column `xxx_id` on table `yyy` does not match column `id` on `zzz`, which has type `bigint(20)`. To resolve this issue, change the type of the `xxx_id` column on `yyy` to be :bigint. (For example `t.bigint :xxx_id`).
    Original message: Mysql2::Error: Cannot add foreign key constraint
    ```

--- db/migrate/20210329000001_create_users.rb
+++ db/migrate/20210329000001_create_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateUsers < ActiveRecord::Migration[6.0]
+class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
