8788604e8b04d0ddfcaf8f97c8d7ec65f2bd65d9
toshimaru <me@toshimaru.net>
2025-04-03 00:22:23 +0900

    feat: Redirect to stored location

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       reset_session
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       log_in(user)
-      redirect_to home_url
+      redirect_to forwarding_url || home_url
     else
       flash.now[:danger] = "Invalid email/password combination"
       render :new
