2cabba9562088af9cd37f642c321f469151bd6ba
Toshimaru <me@toshimaru.net>
2025-07-19 21:22:21 +0900

    Update badges and remove Code Climate references
    Replaced Code Climate badges with Qlty badge in README and removed Code Climate references from test coverage setup in rails_helper.rb. This streamlines project badges and simplifies coverage configuration.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
-# coverage with simplecov and codeclimate
+# coverage with simplecov
 # NOTE: place `SimpleCov.start` before `require 'rspec/rails'`
 require "simplecov"
 SimpleCov.start "rails" do
